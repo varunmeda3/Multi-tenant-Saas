70711632d29527da68a65457d2051674aa393e67
varunmeda3 <medavarun3@gmail.com>
2025-08-26 15:37:48 -0400

    Update fastify.d

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,14 +1,1 @@
-import 'fastify'
-
-import { Member, Organization } from '@prisma/client'
-
-declare module 'fastify' {
-  export interface FastifyRequest {
-    getCurrentUserId(): Promise<string>
-    getUserMembership(organizationSlug: string): Promise<{
-      organization: Organization
-      membership: Member
-    }>
-  }
-}
 
